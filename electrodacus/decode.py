d44e21a002f623bf9fc7c8d1fe8becbf27c6440a
Pi at viflo <pi@viflo>
2021-08-08 08:58:25 +0100

    Latest development

--- electrodacus/decode.py
+++ electrodacus/decode.py
@@ -13,8 +13,12 @@ def main(what, lines):
         if not line.startswith("var ") or not line.endswith('";\n'):
             continue
         key, val = line[4:-3].split('="')
-        if "all" in what: print(key, val)
+        if "all" in what: print(key, "length", len(val), val)
+
+        val = val.replace('\\\\', '\\')
+
         if key == "sbms":
+            assert len(val) == 59
             if "all" in what: print("soc", decode_base91(val[6:8]))
             batt_volt = 0
             for i in range(8):
@@ -30,19 +34,36 @@ def main(what, lines):
             if "amp" in what: print("load.value", (pv_ma - batt_ma) / 1000)
             if "all" in what: print("Status", decode_base91(val[56:59]))
             if "temp" in what: print("temp.value", decode_base91(val[24:26])/10-45)
-
+            if "temp" in what: print("batt.value", decode_base91(val[26:28])/10-45)
+            soc = decode_base91(val[6:8])
+            if "all" in what: print("SOC", soc)
         if key == "xsbms":
+            assert len(val) == 11
             if "all" in what: print("cvmin", decode_base91(val[5:7]))
             if "all" in what: print("cvmax", decode_base91(val[3:5]))
             if "all" in what: print("type", decode_base91(val[7:8]))
-            if "all" in what: print("Cap", decode_base91(val[8:11]))
+            cap = decode_base91(val[8:11])
+            if "all" in what: print("Cap", cap)
         if key == "eW":
+            assert len(val) == 42
             for i in range(8):
                 if "all" in what: print("enW", i, decode_base91(val[6*i:6+6*i]))
+            unit = 360  # .1 Wh to Joule
+            if "watt" in what: print("batt.value", unit * decode_base91(val[0:6]))
+            if "watt" in what: print("pv.value", unit * decode_base91(val[6:12]))
+            if "watt" in what: print("load.value", unit * decode_base91(val[30:36]))
         if key == "eA":
+            assert len(val) == 42
             for i in range(8):
                 if "all" in what: print("enA", i, decode_base91(val[6*i:6+6*i]))
-
+            unit = 3.6  # mAh to Coulomb
+            if "amp" in what: print("batt_coulomb.value", unit * decode_base91(val[0:6]))
+            if "soc" in what: print("soc2.value", .001 * decode_base91(val[0:6]))
+            if "amp" in what: print("pv_coulomb.value", unit * decode_base91(val[6:12]))
+            if "amp" in what: print("load_coulomb.value", unit * decode_base91(val[30:36]))
+    if "soc" in what:
+        # 8000 mAh * 7p = 56 Ah capacity
+        print("soc.value", cap * soc / 100 - cap + 56)
 
 def web_main(what):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -65,14 +86,43 @@ def print_header(what):
         print("graph_title Solar currents")
         print("graph_vlabel Ampere")
         print("graph_category Solar")
-        print("batt.label Battery")
-        print("pv.label Panel")
-        print("load.label Load")
+        print("batt.label Battery Instant")
+        print("pv.label Panel Instant")
+        print("load.label Load Instant")
+        print("batt_coulomb.type DDERIVE")
+        print("pv_coulomb.type DDERIVE")
+        print("load_coulomb.type DDERIVE")
+        print("batt_coulomb.label Battery Average")
+        print("pv_coulomb.label Panel Average")
+        print("load_coulomb.label Load Average")
+        print("batt_coulomb.max 70000")
+        print("pv_coulomb.max 70000")
+        print("load_coulomb.max 70000")
     elif "temp" in what:
         print("graph_title Solar temperature")
         print("graph_vlabel Celcius")
         print("graph_category Solar")
         print("temp.label Attic")
+        print("batt.label Battery")
+    elif "soc" in what:
+        print("graph_title Solar coloumb")
+        print("graph_vlabel Ah")
+        print("graph_category Solar")
+        print("soc.label Battery State of Charge")
+        print("soc2.label Battery Net Charge")
+    elif "watt" in what:
+        print("graph_title Solar energy")
+        print("graph_vlabel W")
+        print("graph_category Solar")
+        print("batt.type DDERIVE")
+        print("pv.type DDERIVE")
+        print("load.type DDERIVE")
+        print("batt.label Battery Average")
+        print("pv.label Panel Average")
+        print("load.label Load Average")
+        print("batt.max 70000")
+        print("pv.max 70000")
+        print("load.max 70000")
 
 
 if __name__ == '__main__':
@@ -81,6 +131,6 @@ if __name__ == '__main__':
         print_header(os.path.basename(sys.argv[0]))
         exit(0)
     if what.endswith(".py"):
-        main("all", sys.stdin)
+        main("allvoltamptempsoc", sys.stdin)
     else:
         web_main(what)
